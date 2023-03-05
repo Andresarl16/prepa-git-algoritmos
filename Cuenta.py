f50e373ab1d6bf4a135138bb6f94d08842fd41ee
AndresARL <andres.rojas5722@gmail.com>
2023-03-05 12:40:36 -0400

    Create class Test and Change Class Cuenta

--- Cuenta.py
+++ Cuenta.py
@@ -1,7 +1,8 @@
 class Cuenta:
-    def __init__(self, titular = None, cantidad = 0):
+    def __init__(self, titular=None, cantidad=0, numeroDeCuenta=0):
         self.titular = titular
         self.cantidad = cantidad
+        self.numeroDeCuenta = numeroDeCuenta
 
     def getTitular(this):
         return this.titular
@@ -15,16 +16,27 @@ class Cuenta:
     def setMonto(this, nuevoMonto):
         this.monto = nuevoMonto
 
+    def getNumeroDeCuenta(this):
+        return this.numeroDeCuenta
+
+    def setNumeroDeCuenta(this, numeroDeCuenta):
+        this.numeroDeCuenta = numeroDeCuenta
+
     def mostrar(this):
         this.titular.mostrar()
-        print("Cantidad: ", this.cantidad)
+
+        if (this.numeroDeCuenta % 2 == 0):
+            this.cantidad *= 2
+        else:
+            this.cantidad += 200
+
+        print("Cantidad: ", this.cantidad, this.numeroDeCuenta)
 
     def ingresar(self, cantidad):
-        if(cantidad > 0):
+        if (cantidad > 0):
             self.cantidad += cantidad
         else:
             print("Ingreso un monto invalido.")
 
     def retirar(self, cantidad):
         self.cantidad -= cantidad
-        
